4b84f36978d90ce863ec7a51930d85fc815f0bb3
Dom-888 <spinelladomenico88@gmail.com>
2020-01-11 14:55:59 +0000

    Add another test

--- spec/calcSpec.js
+++ spec/calcSpec.js
@@ -3,5 +3,9 @@ describe("Calculator", function() {
         it("should return 42", function() {
             expect(addition(20,22)).toBe(42);
         });
+        it("should return 26", function() {
+            expect(addition(9, 17)).toBe(26);
+        })
     });
 });
+
