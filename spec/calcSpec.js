d1056979610b1c1973a39fdaec311ec754b3050b
Dom-888 <spinelladomenico88@gmail.com>
2020-01-12 13:22:01 +0000

    Add new test according to the programming defensively philosophy

--- spec/calcSpec.js
+++ spec/calcSpec.js
@@ -6,6 +6,9 @@ describe("Calculator", function() {
         it("should return 26", function() {
             expect(addition(9, 17)).toBe(26);
         })
+        it("should return an error if we don't supply two numbers", function() {
+            expect(addition("bottle", "water")).toBe(("Error!"))
+        });
     });
 });
 
